40b033f3994aad11ff274192dc4f7bddafd46037
Zubariev <roman.zubariev@wiwi.digital>
2025-05-10 16:57:25 +0300

    fix routers

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,10 +1,7 @@
-import { lazy, type ReactNode, Suspense } from "react";
+import { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import { userRoutes } from "./user-routes";
-
-export const SuspenseWrapper = ({ children }: { children: ReactNode }) => {
-  return <Suspense>{children}</Suspense>;
-};
+import { SuspenseWrapper } from "./utils/suspense";
 
 const NotFoundPage = lazy(() => import("./pages/NotFoundPage"));
 const SomethingWentWrongPage = lazy(
